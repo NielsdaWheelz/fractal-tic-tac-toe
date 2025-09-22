e980be09e7cfd5d2621cf1f587a1d3cd02f348f8
Niels-Erik <nielseirknandal@gmail.com>
2025-09-22 11:56:40 -0400

    init commit, grid border squares

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+       <div className="container">
+        <div className="square" id="one"></div>
+        <div  className="square" id="two"></div>
+        <div  className="square" id="three"></div>
+        <div  className="square" id="four"></div>
+        <div  className="square" id="five"></div>
+        <div  className="square" id="six"></div>
+        <div  className="square" id="seven"></div>
+        <div  className="square" id="eight"></div>
+        <div  className="square" id="nine"></div>
+       </div>
+    </>
+  )
+}
+
+export default App
